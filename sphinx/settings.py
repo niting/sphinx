6fc8311e078265910abc3537d0765dd82ba0ff42
Nitin <nitin.gangahar@gmail.com>
2013-02-13 18:20:16 +0530

    Adding south as an app

--- sphinx/settings.py
+++ sphinx/settings.py
@@ -118,6 +118,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
    	'django.contrib.admin',
     'django.contrib.admindocs',
+	'south',
 )
 
 # A sample logging configuration. The only tangible logging
