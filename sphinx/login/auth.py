ee7a8b9c1373062f782a997ed7472eb31b360792
Samvit <samvit.1@gmail.com>
2013-03-02 06:58:54 -0800

    Updated requirements

--- sphinx/login/auth.py
+++ sphinx/login/auth.py
@@ -2,6 +2,11 @@ from django.contrib.auth.models import User
 from openid.consumer.consumer import SUCCESS
 from django.core.mail import mail_admins
 
+def genpass():
+    import string,random
+    char_set = string.ascii_uppercase + string.digits + '~!@#$%^&*()_+{}:"><?'
+    return ''.join(random.sample(char_set,30))
+
 class GoogleBackend:
     def authenticate(self, openid_response):
         if openid_response is None:
@@ -12,10 +17,13 @@ class GoogleBackend:
         google_email = openid_response.getSigned('http://openid.net/srv/ax/1.0',  'value.email')
         google_firstname = openid_response.getSigned('http://openid.net/srv/ax/1.0', 'value.firstname')
         google_lastname = openid_response.getSigned('http://openid.net/srv/ax/1.0', 'value.lastname')
+        google_email = google_email.split('@')[0]
         try:          
             user = User.objects.get(email=google_email)
         except User.DoesNotExist:
-            user = User.objects.create_user(google_email, google_email, 'password')
+            user = User.objects.create_user(google_email, google_email, genpass())
+            user.first_name = google_firstname
+            user.last_name = google_lastname
             user.save()
             user = User.objects.get(username=google_email)
             return user
