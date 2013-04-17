256ebf1d6d3975c357e4eb868ce4df05a9979144
Nitin <nitin.gangahar@gmail.com>
2013-04-17 10:34:53 +0530

    Making major changes to the model

--- sphinx/models.py
+++ sphinx/models.py
@@ -1,17 +1,38 @@
-#TODO: Change the votes option to not be an integer field but
-#TODO: rather a list of people who upvoted/downvoted
+#TODO : Add commenting features
 from django.db import models
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 from model_utils import Choices
 from django.core.urlresolvers import reverse
 
+class Tag(models.Model):
+	""" Tag ID is a unique hash calculated from the tag string. We
+		have a popularity score too for the popularity of a tag, which
+		would help us with search results and getting maximum matches"""
+	name = models.CharField(max_length=20, unique=True)
+	TAG_TYPE = Choices('college', 'company', 'topic', 'discipline', 'unknown')
+	tag_type = models.CharField(choices=TAG_TYPE,
+								default=TAG_TYPE.unknown, max_length=20)
+	popularity_score = models.IntegerField()	
 
-class Question(models.Model):
+	def __unicode__(self):
+		return self.name
+
+"""Items which are votable come here first"""
+class Votable(models.Model):
+	up_votes = models.IntegerField()
+	down_votes = models.IntegerField()
+	upvotes_by = models.ManyToManyField(User, related_name='%(class)s_upvotes_by')
+	downvotes_by = models.ManyToManyField(User, related_name='%(class)s_downvotes_by')
+	author = models.ForeignKey(User, null=True, blank=True, related_name='%(class)s_author')
+	time = models.DateTimeField(auto_now=True)
+	tags = models.ManyToManyField(Tag, related_name='%(class)s_tags')
+
+	class Meta:
+		abstract = True
+
+
+class Question(Votable):
 	question = models.TextField()
-	asked_by = models.ForeignKey(User, null=True, blank=True, related_name='asked_by')
-	votes = models.IntegerField()
 
 	def get_absolute_url(self):
 		return reverse('sphinx.views.questions_show', args=[str(self.id)])
@@ -19,30 +40,18 @@ class Question(models.Model):
 	def __unicode__(self):
 		return self.question
 
-class Comment(models.Model):
-	comment = models.TextField()
-	comment_by = models.ForeignKey(User)
-	question = models.ForeignKey('Question')
-
-	def __unicode__(self):
-		return self.comment
-
-class Answer(models.Model):
+class Answer(Votable):
 	answer = models.TextField()
-	answered_by = models.ForeignKey(User)
 	question = models.ForeignKey(Question)
-	votes = models.IntegerField(default=0)
 
 	class Meta:
-		unique_together = (("answered_by", "question"),)
+		unique_together = (("author", "question"),)
 
 	def __unicode__(self):
 		return self.answer
 
-class Tip(models.Model):
+class Tip(Votable):
 	tip = models.CharField(max_length=200)
-	tip_by = models.ForeignKey(User)
-	votes = models.IntegerField(default=0)
 
 	def get_absolute_url(self):
 		return reverse('sphinx.views.tips_show', args=[str(self.id)])
@@ -50,19 +59,11 @@ class Tip(models.Model):
 	def __unicode__(self):
 		return self.tip
 
-class Tag(models.Model):
-	""" Tag ID is a unique hash calculated from the tag string. We
-		have a popularity score too for the popularity of a tag, which
-		would help us with search results and getting maximum matches"""
-	name = models.CharField(max_length=20)
-	TAG_TYPE = Choices('college', 'company', 'topic', 'discipline', 'unknown')
-	tag_type = models.CharField(choices=TAG_TYPE,
-								default=TAG_TYPE.unknown, max_length=20)
-
-	question = models.ManyToManyField(Question)
-	tip = models.ManyToManyField(Tip)
-
-	popularity_score = models.IntegerField()	
+class Experience(Votable):
+	experience = models.TextField()
 
 	def __unicode__(self):
-		return self.name
+		return self.experience
+
+"""Votable ends here"""
+
